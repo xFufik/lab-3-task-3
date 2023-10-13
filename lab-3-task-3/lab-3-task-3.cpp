8fdba5c5793ac8b2aea678f1f46202372e262ac1
s8983 <s8983@DESKTOP-QBC14O7>
2023-10-13 11:40:19 +0700

    Добавьте файлы проекта.

---
+++ lab-3-task-3/lab-3-task-3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <clocale>
+#include <cmath>
+
+using namespace std;
+
+/*
+Лабораторная работа 3. Задание 3. Индивидуальный вариант 24.
+Вычислить значение функции y = f(x) при произвольных x.
+*/
+
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    const double a = 2.4;
+    const double b = 1.3;
+    double x, f;
+    cout << "Введите число X: " << endl;
+    cin >> x;
+    if (x < 1) {
+        f = a / x + b * sin(abs(x));
+    }
+    else {
+        if (x >= 1 && x <= 5) {
+            f = log(pow(x + a, 2));
+        }
+        else {
+            f = a * pow(x, 4);
+        }
+    }
+    cout << f << endl;
+}
